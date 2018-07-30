ee10724f71d2efe8d03ea0f3638a64a6d8661ea6
erez <erez.ta@eosos.io>
2018-07-30 09:30:20 +0300

    new API for coin list

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ app.use(cors({origin: 'http://localhost:8080'}));
 var server = app.listen(3000, function () {
     console.log("app running on port.", server.address().port);
 
-
     app.get('/api/coins', (req, res) => {
-        let coins = req.query.coins;
-        console.log(coins);
-        axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+ coins +'&tsyms=USD')
+        console.log(req.query)
+
+        let coinsFrom = req.query.coinsFrom;
+        let coinsTo = req.query.coinsTo;
+        axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + coinsFrom + '&tsyms=' + coinsTo)
+
             .then(response => {
                 let rates = response.data;
                 res.json(rates);
@@ -23,14 +25,27 @@ var server = app.listen(3000, function () {
             .catch(error => {
                 console.log(error);
             });
-    })
+    });
+
+    app.get('/api/coin_list', (req, res) =>{
+        axios.get('https://api.coinmarketcap.com/v2/listings/')
+            .then(response =>{
+                let list = response.data;
+                console.log(list)
+                res.json(list)
+            })
+                    .catch(error =>{
+                        console.log(error)
+
+            })
+    });
 });
-    //yusuf async/await shorthand for get
-    // app.get('/api/btc2', async (req, res) => {
-    //     try {
-    //         let response = await axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR');
-    //         res.send(response.data)
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // });
+//yusuf async/await shorthand for get
+// app.get('/api/btc2', async (req, res) => {
+//     try {
+//         let response = await axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR');
+//         res.send(response.data)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// });
